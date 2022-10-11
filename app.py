04909e5c6840cb1e97d226f75104f7752a4d8ab7
metalpole <junxiong_tan@hotmail.com>
2022-10-12 02:08:20 +0800

    init

--- app.py
+++ app.py
@@ -6,19 +6,31 @@
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
 """
-
-import logging, os, asyncio, io, warnings, datetime
+import logging, os, asyncio, base64
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from PIL import Image
+from torchvision.utils import draw_bounding_boxes
+from torchvision.io import read_image, write_png
+from torch import tensor
+from deepgram import Deepgram
+from google.cloud import aiplatform
 
 load_dotenv()
+dg_client = Deepgram(os.getenv('DEEPGRAM_API'))
+endpoint = aiplatform.Endpoint('projects/73250724104/locations/us-central1/endpoints/4349496475667922944')
 
 # Enable logging
 logging.basicConfig(filename='log.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     filemode='a', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+def encode_image(image):
+    with open(image, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    return encoded_string
+
+colors = ['red','green','cyan','yellow','blue','magenta','black','white']
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -28,16 +40,53 @@ def start(update, context):
 
 def text (update, context):
     """Echo the user message."""
-    update.message.reply_text('This bot only works with audio recordings. Hold down the microphone button to record audio.')
+    update.message.reply_text('This bot detects objects in images. Send over a pic to try it out.')
+
+def image(update, context):
+    # https://stackoverflow.com/questions/50388435/how-save-photo-in-telegram-python-bot
+    image_file = context.bot.get_file(update.message.photo[-1].file_id)
+    image_file.download('image.jpg')
+    update.message.reply_text('Image received, predictions available shortly.')
+
+    image_encoded = encode_image('image.jpg')
+    instance = [{
+        "data": {
+            "b64": str(image_encoded.decode('utf-8'))
+        }
+    }]
+    prediction = endpoint.predict(instances=instance)
+    # print(f"{prediction[0][0]}")
+    im = read_image('image.jpg')
+    boxes, labels = [], [] 
+    for obj in prediction[0][0]:
+        for k in obj:
+            if k != 'score':
+                labels.append(k)
+                boxes.append(obj[k])
+    predictions = draw_bounding_boxes(im, boxes=tensor(boxes), labels=labels, font_size=50, width=3, colors='red')
+    write_png(predictions,'predictions.png')
+    update.message.reply_photo(open('predictions.png', 'rb'))
 
 def voice(update, context):
-    update.message.reply_text('Recording received, image available shortly.')
-    # logger.info(transcript)
+    '''Parse audio recording'''
+    # https://docs.python-telegram-bot.org/en/stable/telegram.message.html?highlight=reply_text#telegram.Message.reply_text
+    # Get audio file
+    audio_file = context.bot.get_file(update.message.voice.file_id)
+    audio_file.download(f"voice_note.ogg")
+    update.message.reply_text('Recording received, transcript available shortly.')
 
-    # Return generated image
-    # update.message.reply_photo(open('image.png', 'rb'), caption=transcript)
-    update.message.reply_text('Recording received, image available shortly.')
+    # Get transcript
+    async def test():
+        with open('voice_note.ogg', 'rb') as audio:
+            source = {'buffer': audio, 'mimetype': 'audio/ogg'}
+            response = await dg_client.transcription.prerecorded(source, {'punctuate': True})
+        return response
+    response = asyncio.run(test())
+    transcript = response['results']['channels'][0]['alternatives'][0]['transcript']
+    logger.info(transcript)
 
+    # Return generated transcript
+    update.message.reply_text(transcript)
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -59,6 +108,7 @@ def main():
 
     # on noncommand i.e message
     dp.add_handler(MessageHandler(Filters.text, text))
+    dp.add_handler(MessageHandler(Filters.photo, image))
     dp.add_handler(MessageHandler(Filters.voice, voice))
 
     # log all errors
